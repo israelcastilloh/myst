bdbf6f1e6081308b2841ed28866b9100ee45b931
ramirezdiana <ramirezdiana721@gmail.com>
2020-11-26 21:47:11 -0600

    Diana: regresion simbolica inicio

--- functions.py
+++ functions.py
@@ -12,8 +12,12 @@
 import numpy as np
 import statsmodels.api as sm
 from scipy.stats import shapiro
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn import svm
+from gplearn.genetic import SymbolicTransformer
 from statsmodels.stats.diagnostic import het_arch
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from visualizations import *
 from data import *
 pd.options.mode.use_inf_as_na = True
@@ -428,3 +432,198 @@ def math_transformations(df):
         df['log2diff_' + col] = np.log2(df[col].values.astype(float))**(1/2)
         df['log10diff_' + col] = np.log10(df[col].values.astype(float))**(1/2)
     return df
+
+# ---------------------------------------------------------- MODEL: Multivariate Linear Regression Model -- #
+# --------------------------------------------------------------------------------------------------------- #
+
+def mult_regression(p_x, p_y):
+    """
+    Funcion para ajustar varios modelos lineales
+
+    Parameters
+    ----------
+
+    p_x: pd.DataFrame
+        with regressors or predictor variables
+        p_x = data_features.iloc[0:30, 3:]
+
+    p_y: pd.DataFrame
+        with variable to predict
+        p_y = data_features.iloc[0:30, 1]
+
+    Returns
+    -------
+    r_models: dict
+        Diccionario con modelos ajustados
+
+    """
+
+    # Fit LINEAR regression
+    linreg = LinearRegression(normalize=False, fit_intercept=False)
+    xtrain, xtest, ytrain, ytest = train_test_split(p_x, p_y, test_size=.8, random_state=455)
+    linreg.fit(xtrain, ytrain)
+    y_p_linear = linreg.predict(xtest)
+    y_p_score = linreg.score(xtest, ytest)
+
+    # Return the result of the model
+    linear_model = {'rss': np.round(sum((y_p_linear - ytest) ** 2), 4),
+                    'predict': ytest,
+                    'model': linreg,
+                    'intercept': linreg.intercept_,
+                    'coef': linreg.coef_,
+                    'score': np.round(y_p_score, 4)}
+    return linear_model
+
+
+# -------------------------------- MODEL: Multivariate Linear Regression Models with L1L2 regularization -- #
+# --------------------------------------------------------------------------------------------------------- #
+
+def mult_reg_l1l2(p_x, p_y, p_alpha, p_iter):
+    """
+    Funcion para ajustar varios modelos lineales
+
+    Parameters
+    ----------
+
+    p_x: pd.DataFrame
+        with regressors or predictor variables
+        p_x = data_features.iloc[0:30, 3:]
+
+    p_y: pd.DataFrame
+        with variable to predict
+        p_y = data_features.iloc[0:30, 1]
+
+    p_alpha: float
+        alpha for the models
+        p_alpha = alphas[1e-3]
+
+    p_iter: int
+        Number of iterations until stop the model fit process
+        p_iter = 1e6
+
+    Returns
+    -------
+    r_models: dict
+        Diccionario con modelos ajustados
+
+    """
+    xtrain, xtest, ytrain, ytest = train_test_split(p_x, p_y, test_size=.8, random_state=455)
+    # Fit RIDGE regression
+    ridgereg = Ridge(alpha=p_alpha, normalize=False, max_iter=p_iter, fit_intercept=False)
+    ridgereg.fit(xtrain, ytrain)
+    y_p_ridge = ridgereg.predict(xtest)
+
+    # Fit LASSO regression
+    lassoreg = Lasso(alpha=p_alpha, normalize=False, max_iter=p_iter, fit_intercept=False)
+    lassoreg.fit(xtrain, ytrain)
+    y_p_lasso = lassoreg.predict(xtest)
+
+    # Fit ElasticNet regression
+    enetreg = ElasticNet(alpha=p_alpha, normalize=False, max_iter=p_iter, l1_ratio=0.5, fit_intercept=False)
+    enetreg.fit(xtrain, ytrain)
+    y_p_enet = enetreg.predict(xtest)
+
+    # RSS = residual sum of squares
+
+    # Return the result of the model
+    r_models = {'rige': {'rss': sum((y_p_ridge - ytest) ** 2),
+                         'predict': y_p_ridge,
+                         'model': ridgereg,
+                         'intercept': ridgereg.intercept_,
+                         'coef': ridgereg.coef_},
+                'lasso': {'rss': sum((y_p_lasso - ytest) ** 2),
+                          'predict': y_p_lasso,
+                          'model': lassoreg,
+                          'intercept': lassoreg.intercept_,
+                          'coef': lassoreg.coef_},
+                'elasticnet': {'rss': sum((y_p_enet - ytest) ** 2),
+                               'predict': y_p_enet,
+                               'model': enetreg,
+                               'intercept': enetreg.intercept_,
+                               'coef': enetreg.coef_}
+                }
+
+    return r_models
+
+# ------------------------------------------------------------------ MODEL: Symbolic Features Generation -- #
+# --------------------------------------------------------------------------------------------------------- #
+
+def symbolic_features(p_x, p_y):
+    """
+    Funcion para crear regresores no lineales
+
+    Parameters
+    ----------
+    p_x: pd.DataFrame
+        with regressors or predictor variables
+        p_x = data_features.iloc[0:30, 3:]
+
+    p_y: pd.DataFrame
+        with variable to predict
+        p_y = data_features.iloc[0:30, 1]
+
+    Returns
+    -------
+    score_gp: float
+        error of prediction
+
+    """
+
+    model = SymbolicTransformer(function_set=["sub", "add", 'inv', 'mul', 'div', 'abs', 'log'],
+                                population_size=5000, hall_of_fame=100, n_components=20,
+                                generations=20, tournament_size=20,  stopping_criteria=.05,
+                                const_range=None, init_depth=(4, 12),
+                                metric='pearson', parsimony_coefficient=0.001,
+                                p_crossover=0.4, p_subtree_mutation=0.2, p_hoist_mutation=0.1,
+                                p_point_mutation=0.3, p_point_replace=.05,
+                                verbose=1, random_state=None, n_jobs=-1, feature_names=p_x.columns,
+                                warm_start=True)
+
+    model_fit = model.fit_transform(p_x, p_y)
+    model_params = model.get_params()
+    results = {'fit': model_fit, 'params': model_params, 'model': model}
+
+    return results
+
+
+def f_features(p_data, p_nmax):
+    # reasignar datos
+    data = p_data.copy()
+    data["predict"] = data['Close'].shift(-1)
+    # pips descontados al cierre
+    data['co'] = (data['Close'] - data['Open']) * 10000
+    # pips descontados alcistas
+    data['ho'] = (data['High'] - data['Open']) * 10000
+    # pips descontados bajistas
+    data['ol'] = (data['Open'] - data['Low']) * 10000
+    # pips descontados en total (medida de volatilidad)
+    data['hl'] = (data['High'] - data['Low']) * 10000
+
+    for n in range(0, p_nmax):
+
+        # rezago n de Open - Low
+        data['lag_ol_' + str(n + 1)] = data['ol'].shift(n + 1)
+        # rezago n de High - Open
+        data['lag_ho_' + str(n + 1)] = data['ho'].shift(n + 1)
+        # rezago n de High - Low
+        data['lag_hl_' + str(n + 1)] = data['hl'].shift(n + 1)
+
+
+        # promedio movil de open-high de ventana n
+        data['ma_ol_' + str(n + 2)] = data['ol'].rolling(n + 2).mean()
+        # promedio movil de ventana n
+        data['ma_ho_' + str(n + 2)] = data['ho'].rolling(n + 2).mean()
+        # promedio movil de ventana n
+        data['ma_hl_' + str(n + 2)] = data['hl'].rolling(n + 2).mean()
+
+        # hadamard product of previously generated features
+        list_hadamard = [data['lag_ol_' + str(n + 1)], data['lag_ho_' + str(n + 1)],
+                         data['lag_hl_' + str(n + 1)]]
+
+        for x in list_hadamard:
+            data['had_' + 'lag_oi_' + str(n + 1) + '_' + 'ma_ol_' + str(n + 2)] = x * data['ma_ol_' + str(n + 2)]
+            data['had_' + 'lag_oi_' + str(n + 1) + '_' + 'ma_ho_' + str(n + 2)] = x * data['ma_ho_' + str(n + 2)]
+            data['had_' + 'lag_oi_' + str(n + 1) + '_' + 'ma_hl_' + str(n + 2)] = x * data['ma_hl_' + str(n + 2)]
+    data = data.dropna(axis='rows')
+    data = data.drop(['Open', 'High', 'Low', 'Close'], axis=1)
+    return data
--- main.py
+++ main.py
@@ -18,10 +18,10 @@ from data import *
 Datos históricos de divisa
 '''
 
-# save_pkl('USD_MXN')
+
 datos_divisa = read_pkl('USD_MXN')  # 4HRS --> USD/MXN - Mexican Peso
 # print(datos_divisa)
-
+features_divisa=ft.f_features(datos_divisa, 3)
 # -- ---------------------------------------------------------------------------------------------------------------- #
 '''--------------------------------------------------------------
 Partición de datos en train y test
@@ -48,11 +48,43 @@ estadisticos = ft.get_dfestadisticos(estacionaridad, autocorrelacion, normalidad
 Crear variables artificiales - indicadores financieros y estadísticos
 '''
 
-datos_divisa = ft.add_all_features(datos_divisa)
+#features_divisa = ft.add_all_features(datos_divisa)
 
 '''--------------------------------------------------------------
 crear variables artificiales - transformaciones matemáticas y change point
 '''
 
-datos_divisa = ft.math_transformations(datos_divisa)
+#datos_divisa = ft.math_transformations(datos_divisa)
+
+cor_mat = features_divisa.iloc[:, :].corr()
+
+lm_model = ft.mult_regression(p_x=features_divisa.iloc[:, 1:], p_y=features_divisa.iloc[:, 0])
+
+# RSS of the model with all the variables
+print('Modelo Lineal 1: rss: ', lm_model['rss'])
+
+# R^2 of the model
+print('Modelo Lineal 1: score: ', lm_model['score'])
+
+
+# Generacion de un feature formado con variable simbolica
+symbolic = ft.symbolic_features(p_x=features_divisa.iloc[:, 1:], p_y=features_divisa.iloc[:, 0])
+
+#symbolic['model']._best_programs[3].__str__()
+
+# -- Transformer -- #
+nuevos_features = pd.DataFrame(symbolic['fit'], index=features_divisa.index)
+nuevos_features_c = pd.concat([features_divisa, nuevos_features], axis=1)
+
+# -- ---------------------------------------------------------------------------------------- Models fit -- #
+cor_mat = nuevos_features_c.iloc[:, :].corr()
+# Multple linear regression model
+lm_model_s = ft.mult_regression(p_x=nuevos_features_c.iloc[:, 1:], p_y=nuevos_features_c.iloc[:, 0])
+
+# RSS of the model with all the variables
+print('Modelo Lineal 2: rss: ', lm_model_s['rss'])
+# R^2 of the model
+print('Modelo Lineal 2: score: ', lm_model_s['score'])
+
+
 
